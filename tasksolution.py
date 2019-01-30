3621befc82d6a9cb2919c80e2e7ead5f230fc237
patelnancy12 <Nancy.Patel@dal.ca>
2019-01-30 18:41:14 +0000

    Task performed

--- tasksolution.py
+++ tasksolution.py
@@ -1,6 +1,8 @@
 """This program is to traverse in a string"""
 
 import os
-path = os.getcwd()
-print("Path:",path)
-print("Reverse:", Path[::-1]
+path = "."
+
+files = os.listdir(path)
+for name in files:
+  print(name)
